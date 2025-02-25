c90dec261afe07badfb9cbfbf0c04ce199ce8c16
axr1023 <axr1023@student.bham.ac.uk>
2025-02-25 01:59:43 +0100

    removed firebase

--- src/firebase.js
+++ src/firebase.js
@@ -2,15 +2,6 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
-// const firebaseConfig = {
-//   apiKey: "",
-//   authDomain: "ongcetbulgaria.firebaseapp.com",
-//   projectId: "ongcetbulgaria",
-//   storageBucket: "ongcetbulgaria.firebasestorage.app",
-//   messagingSenderId: "1096144210485",
-//   appId: "1:1096144210485:web:a2376a7e713c2e05d2bf7c"
-// };
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
